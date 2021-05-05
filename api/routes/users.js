125c388dc8cb6577ff753f60464596892712a502
john sanchez <fsanchezvanegas@gmail.com>
2021-05-05 12:36:50 -0500

    Crear usuari con contraseña encriptada en mongo clase 118

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,13 +2,51 @@ const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken"); //con esta libreria encriptamos las contrasenas
 const bcrypt = require("bcrypt");
-
+  
 //models import
 import User from "../models/user.js";
 
-//AUTH
-router.post("/register", (req, res) => {
+//POST -> req.body
+//GET -> req.query
 
+//AUTH
+router.post("/register", async (req, res) => {
+  try {
+
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+    const encryptedPassword = bcrypt.hashSync(password, 10);
+
+    const newUser = {
+      name: name,
+      email: email,
+      password: encryptedPassword
+    };
+
+    var user = await User.create(newUser);
+
+    console.log(user);
+
+    const toSend = {
+      status: "success"
+    };
+
+    res.status(200).json(toSend);
+
+  } catch (error) {
+      
+      console.log("ERROR - REGISTER ENDPOINT")
+      console.log(error);
+
+      const toSend = {
+        status: "error",
+        error: error
+      };
+
+      res.status(500).json(toSend);
+
+  }
 });
 
 router.post("/login", (req, res) => {
